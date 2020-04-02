42e7b95546396e154c9a12b16fc47156f95f7746
EVAKuznetsov <47134052+EVAKuznetsov@users.noreply.github.com>
2020-04-02 12:38:34 +1000

    добавленны eslint и prettier

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,109 +1,107 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import FormAddPoint from '../form-add-point'
-import PointsList  from '../points-list'
+import PointsList from '../points-list'
 import Map from '../map'
 
-
 export default class App extends Component {
-  id=1;
-  state={
-    mapCenter:[55.75,37.62],
-    points:[
-        {
-          id:990,
-          label:'Первая точка',
-          position:[55.75,37.68]        
-        },
-        {
-          id:991,
-          label:'Вторая точка',
-          position:[55.77,37.61]        
-        },
-        {
-          id:992,
-          label:'Третья точка',
-          position:[55.71, 37.61]        
-        }
-      // this.createPoint('Первая точка'),
-      // this.createPoint('Вторая точка'),
-      // this.createPoint('четвёртая точка'),
-      // this.createPoint('Третья точка')
-    ]
-  }
-  //создаём новую точку
-  createPoint(label){
-    const center = [...this.state.mapCenter];
-    return{
-      id:this.id++,
-      label,
-      position:center
+    id = 1
+    state = {
+        mapCenter: [55.75, 37.62],
+        points: [
+            {
+                id: 990,
+                label: 'Первая точка',
+                position: [55.75, 37.68],
+            },
+            {
+                id: 991,
+                label: 'Вторая точка',
+                position: [55.77, 37.61],
+            },
+            {
+                id: 992,
+                label: 'Третья точка',
+                position: [55.71, 37.61],
+            },
+            // this.createPoint('Первая точка'),
+            // this.createPoint('Вторая точка'),
+            // this.createPoint('четвёртая точка'),
+            // this.createPoint('Третья точка')
+        ],
     }
-  }
-  //получаем координаты точки и перезаписываем в state
-  onChangePosition=(position,id)=>{
-    this.setState(({points})=>{
-      const idx = points.findIndex(point=>point.id===id);
-      const newPoints = [...points];
-      newPoints[idx].position = position;
-      return (
-        {points:newPoints}
-      )
-    })    
-  }
-  //получаем список точек с новым порядком элементов внутри и перезаписываем
-  onChangeOderPoints=(points)=>{
-    this.setState({points})
-  }
-  //получаем координаты центра карты и перезаписываем в state
-  onChangeMapCenter=(mapCenter)=>{
-    this.setState({mapCenter});
-  }
-  //добавляем точку в state с переданным новым именем
-  addPoint=(label)=>{
-    const newPoint = this.createPoint(label);//вызываем функцию создания новой точки
-    this.setState(({points})=>{
-      const newPointList=[...points,newPoint];
-      return {points:newPointList}
-    });
-  }
-  //удаляем точку из state-ов по переданному id
-  delPoint = (id)=>{
-    console.log(`удалить${id}`);
-    this.setState(({points})=>{
-        const idx = points.findIndex((el)=>el.id===id);
-        const newPoints = [...points];
-        newPoints.splice(idx,1);
-        return{points:newPoints}
-      })
-  }
+    //создаём новую точку
+    createPoint(label) {
+        const center = [...this.state.mapCenter]
+        return {
+            id: this.id++,
+            label,
+            position: center,
+        }
+    }
+    //получаем координаты точки и перезаписываем в state
+    onChangePosition = (position, id) => {
+        this.setState(({ points }) => {
+            const idx = points.findIndex(point => point.id === id)
+            const newPoints = [...points]
+            newPoints[idx].position = position
+            return { points: newPoints }
+        })
+    }
+    //получаем список точек с новым порядком элементов внутри и перезаписываем
+    onChangeOderPoints = points => {
+        this.setState({ points })
+    }
+    //получаем координаты центра карты и перезаписываем в state
+    onChangeMapCenter = mapCenter => {
+        this.setState({ mapCenter })
+    }
+    //добавляем точку в state с переданным новым именем
+    addPoint = label => {
+        const newPoint = this.createPoint(label) //вызываем функцию создания новой точки
+        this.setState(({ points }) => {
+            const newPointList = [...points, newPoint]
+            return { points: newPointList }
+        })
+    }
+    //удаляем точку из state-ов по переданному id
+    delPoint = id => {
+        console.log(`удалить${id}`)
+        this.setState(({ points }) => {
+            const idx = points.findIndex(el => el.id === id)
+            const newPoints = [...points]
+            newPoints.splice(idx, 1)
+            return { points: newPoints }
+        })
+    }
 
-  render(){
-    const {points,mapCenter} = this.state;
-    return (
-      
-      <div className="container mt-4">
-        <div className="row jumbotron p-5">
-        
-          <div className="col-md-4 mb-4">            
-            <div>
-              <FormAddPoint addPoint={this.addPoint}/>
+    render() {
+        const { points, mapCenter } = this.state
+        return (
+            <div className="container mt-4">
+                <div className="row jumbotron p-5">
+                    <div className="col-md-4 mb-4">
+                        <div>
+                            <FormAddPoint addPoint={this.addPoint} />
+                        </div>
+                        <div>
+                            <PointsList
+                                points={points}
+                                delPoint={this.delPoint}
+                                onChangeOderPoints={this.onChangeOderPoints}
+                            />
+                        </div>
+                    </div>
+                    <div className="col-md-8">
+                        <Map
+                            points={points}
+                            mapCenter={mapCenter}
+                            onChangePosition={this.onChangePosition}
+                            onChangeMapCenter={this.onChangeMapCenter}
+                        />
+                    </div>
+                </div>
             </div>
-            <div>
-              <PointsList points = {points} 
-                  delPoint = {this.delPoint}
-                  onChangeOderPoints={this.onChangeOderPoints}/>
-            </div>              
-          </div>
-          <div className="col-md-8">
-            <Map points ={points} mapCenter={mapCenter} 
-                onChangePosition={this.onChangePosition}
-                onChangeMapCenter={this.onChangeMapCenter}/>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        )
+    }
 }
-
-
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,85 +1,81 @@
-import React,{Component} from 'react'
-import{YMaps, Map, Placemark, Polyline} from 'react-yandex-maps'
+import React, { Component } from 'react'
+import { YMaps, Map, Placemark, Polyline } from 'react-yandex-maps'
 export default class MapContainer extends Component {
-    state = {        
-        points:[]
-    };
-    //проверяем на изменение входящих props и перерисовываем компонент в случае несоответствия
-    componentDidUpdate(prevProps){
-        if(prevProps.points!==this.props.points){
-            this.setState({
-                points:this.props.points
-            })
-        }
-    }
-    //при прорисовки компонента, проверяем на заполненость массива точек в состоянии данного компонента и в случае, если массив пустой, то перезаписываем в state массив из props-ов
-    componentDidMount(){
-        if(this.state.points.length===0){
-            this.setState({points:this.props.points})
-        }
-    } 
-    //получаем координаты точки после перетаскивания и отправляем на перезапись в компоненту App
-    onMarkerDragEnd =(e,id)=>{
-        const position = e.originalEvent.target.geometry._coordinates
-        this.props.onChangePosition(position,id);
-    }
-    //получаем координаты центра карты при её смещении и отправляем на перезапись
-    onBoundsChange=(map)=>{
-        this.props.onChangeMapCenter(map._cache.newCenter);
-    }
-
-    render(){
-        //добавляем размеры для карты
-        const style = {
-            width: '100%',
-            height: '400px'
-        }
-        //получаем список координат точек для отрисовки линий
-        const routes = this.state.points.map(marker=>marker.position);
-        //создаём массив маркеров
-        const markers = this.state.points.map((point,i)=>{
-            return(
-                <Placemark 
-                    key = {point.id} 
-                    geometry={point.position}
-                    options={{
-                        draggable:true                        
-                    }}
-                    properties={{
-                        hintContent:point.label,
-                        balloonContent:point.label
-                    }}
-                    modules= {
-                        ['geoObject.addon.balloon', 'geoObject.addon.hint']
-                    }  
-                    onDragend={(e)=>this.onMarkerDragEnd(e,point.id)}                  
-                />
-            )
-        })
-        
-        return (
-            <YMaps>
-                <div>
-                <Map 
-                    {...style} 
-                    defaultState={{ center: this.props.mapCenter, zoom: 9}}
-                    onBoundsChange={map=>this.onBoundsChange(map)}
-                    >
-                    {markers}
-                    <Polyline
-                        geometry={routes}
-                        options={{
-                            balloonCloseButton: false,
-                            strokeColor: '#000',
-                            strokeWidth: 4,
-                            strokeOpacity: 0.5,
-                        }}
-                    />
-                </Map>
-                
-                </div>
-            </YMaps>
-        );
+  state = {
+    points: [],
+  }
+  //проверяем на изменение входящих props и перерисовываем компонент в случае несоответствия
+  componentDidUpdate(prevProps) {
+    if (prevProps.points !== this.props.points) {
+      this.setState({
+        points: this.props.points,
+      })
     }
   }
+  //при прорисовки компонента, проверяем на заполненость массива точек в состоянии данного компонента и в случае, если массив пустой, то перезаписываем в state массив из props-ов
+  componentDidMount() {
+    if (this.state.points.length === 0) {
+      this.setState({ points: this.props.points })
+    }
+  }
+  //получаем координаты точки после перетаскивания и отправляем на перезапись в компоненту App
+  onMarkerDragEnd = (e, id) => {
+    const position = e.originalEvent.target.geometry._coordinates
+    this.props.onChangePosition(position, id)
+  }
+  //получаем координаты центра карты при её смещении и отправляем на перезапись
+  onBoundsChange = (map) => {
+    this.props.onChangeMapCenter(map._cache.newCenter)
+  }
 
+  render() {
+    //добавляем размеры для карты
+    const style = {
+      width: '100%',
+      height: '400px',
+    }
+    //получаем список координат точек для отрисовки линий
+    const routes = this.state.points.map((marker) => marker.position)
+    //создаём массив маркеров
+    const markers = this.state.points.map((point, i) => {
+      return (
+        <Placemark
+          key={point.id}
+          geometry={point.position}
+          options={{
+            draggable: true,
+          }}
+          properties={{
+            hintContent: point.label,
+            balloonContent: point.label,
+          }}
+          modules={['geoObject.addon.balloon', 'geoObject.addon.hint']}
+          onDragend={(e) => this.onMarkerDragEnd(e, point.id)}
+        />
+      )
+    })
+
+    return (
+      <YMaps>
+        <div>
+          <Map
+            {...style}
+            defaultState={{ center: this.props.mapCenter, zoom: 9 }}
+            onBoundsChange={(map) => this.onBoundsChange(map)}
+          >
+            {markers}
+            <Polyline
+              geometry={routes}
+              options={{
+                balloonCloseButton: false,
+                strokeColor: '#000',
+                strokeWidth: 4,
+                strokeOpacity: 0.5,
+              }}
+            />
+          </Map>
+        </div>
+      </YMaps>
+    )
+  }
+}
